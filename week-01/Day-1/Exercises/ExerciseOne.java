7b607d62e40898b2340ed427c27e3eb47b5d9e7d
Hana Debay <hana2024ug@gmail.com>
2025-11-19 23:13:56 +0300

    Day 1: Exercise1 OOP-Basic

---
+++ week-01/Day-1/Exercises/ExerciseOne.java
@@ -0,0 +1,20 @@
+public class ExerciseOne {
+    public static void main(String[] args) {
+        Person person1 = new Person("Hana", 38);
+        Person person2 = new Person("Abel", 39);
+        person1.introduce();
+        person2.introduce();
+    }
+}
+class Person{
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void introduce(){
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+}
